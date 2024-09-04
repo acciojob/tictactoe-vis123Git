6ebc07b293eb615be930a4a035d52914b1b98042
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-09-04 22:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function startGame(e) {
+	e.preventDefault()
+	const p1name = document.getElementById("player-1");
+	const p2name = document.getElementById("player-2");
+	const container = document.getElementsByClassName("container")[0];
+	if(p1name && p2name){
+		for (let i = 1; i < 10; i++) {
+			const div = document.createElement("div")
+			div.id = i;
+		}
+	}
+
+}
